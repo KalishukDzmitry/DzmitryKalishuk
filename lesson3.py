dfe1b9cf3aec2a54e56b56a64dc51f0e32f84c74
DzmitryKalishuk <kselian5567@gmail.com>
2023-12-10 17:05:08 +0300

    read me

--- lesson3.py
+++ lesson3.py
@@ -3,5 +3,10 @@ weight = float(input("Введите свой вес в кг: "))
 height = height / 100
 BMI = weight / (height * height)
 print("Ваш индекс массы тела равен: ", BMI)
-scale = "20" + "=" * int(BMI - 20) + "|" + "=" * int(50 - BMI) + "50"
+scale = "18.5" + "=" * int(BMI - 18.5) + "|" + "=" * int(24.9 - BMI) + "24.9"
 print(scale)
+
+weight = 80.4
+height = 181
+
+BMI 
